514a9e2e8b83a062d4c7ade914e042539b1a6ab2
shinkiyeol9814-droid <shinkiyeol9814@gmail.com>
2026-04-22 22:35:09 +0900

    Update batch_report.py

--- batch_report.py
+++ batch_report.py
@@ -15,7 +15,7 @@ API_ID = int(os.environ.get("TELEGRAM_API_ID", 0))
 API_HASH = os.environ.get("TELEGRAM_API_HASH", "")
 SESSION_STR = os.environ.get("TELEGRAM_SESSION", "")
 # 💡 B계정(새 API 키) 사용
-GEMINI_KEY = os.environ.get("GEMINI_API_KEY_B", "") 
+GEMINI_KEY = os.environ.get("GEMINI_API_KEY_A", "") 
 
 client_ai = genai.Client(api_key=GEMINI_KEY)
 
